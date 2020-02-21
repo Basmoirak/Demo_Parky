04f6a0a24871772c0377459f8744b426da6ed014
basmoirak <daniel_yn@yahoo.com.sg>
2020-02-21 15:50:36 +0800

    Added National Parks Web UI.

--- ParkyWeb/SD.cs
+++ ParkyWeb/SD.cs
@@ -8,7 +8,7 @@ namespace ParkyWeb
     public static class SD
     {
         public static string APIBaseUrl = "https://localhost:44305/";
-        public static string NationalParkAPIPath = APIBaseUrl + "api/nationalparks";
-        public static string TrailAPIPath = APIBaseUrl + "api/trails";
+        public static string NationalParkAPIPath = APIBaseUrl + "api/nationalparks/";
+        public static string TrailAPIPath = APIBaseUrl + "api/trails/";
     }
 }
